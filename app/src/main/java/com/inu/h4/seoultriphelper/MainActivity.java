3e59ef48ff610e517209e4215f985391e50dbb86
MIN <dhmin5693@naver.com>
2016-08-24 21:07:17 +0900

    Fragment로 변경

--- app/src/main/java/com/inu/h4/seoultriphelper/MainActivity.java
+++ app/src/main/java/com/inu/h4/seoultriphelper/MainActivity.java
@@ -1,26 +1,38 @@
 package com.inu.h4.seoultriphelper;
 
-import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.design.widget.NavigationView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
-public class PageHomeActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-    Button tempDetailButton;
+
+    Fragment pageHomeFragment;
+    Fragment pagePreferEmptyFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.page_home);
+        setContentView(R.layout.activity_main);
 
+        // 각 페이지에 해당하는 Fragment 초기화
+        pageHomeFragment = new PageHomeFragment();
+        pagePreferEmptyFragment = new PagePreferEmptyFragment();
 
+        // 초기 화면으로 사용할 fragment 설정
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.container, pageHomeFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+
+        // 앱 최상단에 메뉴, 검색버튼과 화면 이름을 출력하는 툴바를 생성
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -31,44 +43,35 @@ public class PageHomeActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-
-        tempDetailButton = (Button) findViewById(R.id.btn_temp_detail);
-        tempDetailButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(PageHomeActivity.this, PageSightDetailActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         int id=item.getItemId();
-        Intent intent = null;
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 
         if (id == R.id.nav_home) {
-            // Handle the camera action
+            transaction.replace(R.id.container, pageHomeFragment);
         } else if (id == R.id.nav_tag) {
 
         } else if (id == R.id.nav_prefer) {
             if(true) {          // 설문 결과가 없을 경우
-                intent = new Intent(PageHomeActivity.this, PagePreferEmptyActivity.class);
+                transaction.replace(R.id.container, pagePreferEmptyFragment);
             } else {            // 설문 결과가 있을 경우
-                intent = new Intent(PageHomeActivity.this, PagePreferExistActivity.class);
+
             }
         } else if (id == R.id.nav_bucket) {
             if(true) {          // 장바구니가 비어있을 경우
-                intent = new Intent(PageHomeActivity.this, PageBucketEmptyActivity.class);
+
             } else {            // 장바구니가 비어있지 않은 경우
-                intent = new Intent(PageHomeActivity.this, PageBucketExistActivity.class);
+
             }
         } else if (id == R.id.nav_planner) {
             if(true) {          // 플래너가 비어있을 경우
-                intent = new Intent(PageHomeActivity.this, PagePlannerEmptyActivity.class);
+
             } else {            // 플래너가 비어있지 않은 경우
-                intent = new Intent(PageHomeActivity.this, PagePlannerExistActivity.class);
+
             }
 
         } else if (id == R.id.nav_map) {
@@ -76,13 +79,13 @@ public class PageHomeActivity extends AppCompatActivity
         } else if (id == R.id.nav_setting) {
 
         }
-        if(intent != null) {
-            startActivity(intent);
-        }
+
+        transaction.addToBackStack(null);
+        transaction.commit();
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
 
         return true;
     }
-
 }

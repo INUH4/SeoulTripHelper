e98319f22ee6a984819607d63eb1f40a95b2006e
MIN <dhmin5693@naver.com>
2016-08-27 17:40:22 +0900

    홈 화면 탭 생성.
    수정이 필요함

--- app/src/main/java/com/inu/h4/seoultriphelper/PageHomeFragment.java
+++ app/src/main/java/com/inu/h4/seoultriphelper/PageHomeFragment.java
@@ -2,15 +2,25 @@ package com.inu.h4.seoultriphelper;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PageHomeFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    Fragment weeklyRanking;
+    Fragment monthlyRanking;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -48,12 +58,53 @@ public class PageHomeFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View layout = inflater.inflate(R.layout.page_home, container, false);
 
+        Fragment weeklyRanking = new HomeRankingFragment();
+        Fragment monthlyRanking = new HomeRankingFragment();
+
+        final List<Fragment> fragments = new ArrayList<>();
+        fragments.add(weeklyRanking);
+        fragments.add(monthlyRanking);
+
+        FragmentPagerAdapter adapter = new FragmentPagerAdapter(getActivity().getSupportFragmentManager()) {
+            /*
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return fragments.get(position).getTitle(getActivity());
+            }
+            */
+            @Override
+            public Fragment getItem(int position) {
+                return fragments.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return fragments.size();
+            }
+        };
+
+        //getActivity().setTitle(fragments.get(0).getTitle(getActivity()));
+        ViewPager pager = (ViewPager) layout.findViewById(R.id.home_viewpager);
+        pager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                //getActivity().setTitle(fragments.get(position).getTitle(getActivity()));
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+        pager.setAdapter(adapter);
+        ((SmartTabLayout) layout.findViewById(R.id.home_viewpagertab)).setViewPager(pager);
         getActivity().setTitle("홈 화면");
-
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.prefer_empty, container, false);
+        return layout;
     }
 }
